d9988d1164755b0a01a69db6738cd163b2894c58
safa-abidi <safa.abidi99@gmail.com>
2023-05-31 17:04:22 +0100

    sanitize routers

--- routes/author.js
+++ routes/author.js
@@ -27,7 +27,7 @@ const mystorage = multer.diskStorage({
 const upload = multer({storage: mystorage})
 
 
-router.post('/register' , upload.any('image') , (req, res)=>{
+/*router.post('/register' , upload.any('image') , (req, res)=>{
 
     data = req.body;
     author = new Author(data);
@@ -50,12 +50,32 @@ router.post('/register' , upload.any('image') , (req, res)=>{
                 res.send(err)
             }
         )
+})*/
+
+router.post('/register', upload.any('image'), (req, res) => {
+    let data = req.body;
+    let author = new Author(data);
+  
+    author.image = filename;
+  
+    let salt = bcrypt.genSaltSync(10);
+    author.password = bcrypt.hashSync(data.password, salt);
+  
+    author.save()
+      .then((savedAuthor) => {
+        filename = '';
+        // Sanitize the 'savedAuthor' variable before sending it in the response
+        const sanitizedData = sanitize(savedAuthor); // Replace 'sanitize' with the appropriate function
+        res.status(200).send(sanitizedData);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+  
 
 
-})
-
-
-router.post('/login' , (req, res)=>{
+/*router.post('/login' , (req, res)=>{
     
     let data = req.body;
 
@@ -80,21 +100,48 @@ router.post('/login' , (req, res)=>{
                 }
 
             }
-
-
         )
         .catch(
             err=>{
                 res.send(err);
             }
         )
+})*/
+
+router.post('/login', (req, res) => {
+    let data = req.body;
+  
+    Author.findOne({ email: data.email })
+      .then((author) => {
+        if (!author) {
+          res.send('email or password invalid');
+        } else {
+          let valid = bcrypt.compareSync(data.password, author.password);
+          if (!valid) {
+            res.send('email or password invalid');
+          } else {
+            let payload = {
+              _id: author._id,
+              email: author.email,
+              fullname: author.name + ' ' + author.lastname,
+            };
+  
+            let token = jwt.sign(payload, '123456789');
+  
+            // Sanitize the 'token' variable before sending it in the response
+            const sanitizedToken = sanitize(token); // Replace 'sanitize' with the appropriate function
+            res.send({ myToken: sanitizedToken });
+          }
+        }
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+  
 
 
-
-})
-
-
-router.get('/all' , (req, res)=>{
+/*router.get('/all' , (req, res)=>{
     
     Author.find({})
     .then(
@@ -107,10 +154,22 @@ router.get('/all' , (req, res)=>{
             res.status(400).send(err);
         }
     )
+})*/
 
-})
+router.get('/all', (req, res) => {
+    Author.find({})
+      .then((authors) => {
+        // Sanitize the 'authors' variable before sending it in the response
+        const sanitizedData = sanitize(authors); // Replace 'sanitize' with the appropriate function
+        res.status(200).send(sanitizedData);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  });
+  
 
-router.get('/getbyid/:id' , (req, res)=>{
+/*router.get('/getbyid/:id' , (req, res)=>{
     let id = req.params.id
 
     Author.findOne({ _id: id })
@@ -124,9 +183,24 @@ router.get('/getbyid/:id' , (req, res)=>{
             res.status(400).send(err);
         }
     )
-})
+})*/
 
-router.delete('/supprimer/:id' , (req, res)=>{
+router.get('/getbyid/:id', (req, res) => {
+    let id = req.params.id;
+  
+    Author.findOne({ _id: id })
+      .then((author) => {
+        // Sanitize the 'author' variable before sending it in the response
+        const sanitizedData = sanitize(author); // Replace 'sanitize' with the appropriate function
+        res.status(200).send(sanitizedData);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  });
+  
+
+/*router.delete('/supprimer/:id' , (req, res)=>{
     let id = req.params.id
 
     Author.findByIdAndDelete({_id: id})
@@ -140,7 +214,22 @@ router.delete('/supprimer/:id' , (req, res)=>{
                 res.status(400).send(err);
             }
         )
-})
+})*/
+
+router.delete('/supprimer/:id', (req, res) => {
+    let id = req.params.id;
+  
+    Author.findByIdAndDelete({ _id: id })
+      .then((author) => {
+        // Sanitize the 'author' variable before sending it in the response
+        const sanitizedData = sanitize(author); // Replace 'sanitize' with the appropriate function
+        res.status(200).send(sanitizedData);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  });
+  
 
 
 
